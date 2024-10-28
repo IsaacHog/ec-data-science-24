1f9cdfdce0fa7ab3cc3b91f8ab1cac18d6130a7c
IsaacHog <isaac.hogfeldt@hotmail.com>
2024-10-28 17:11:26 +0100

    fix(statistisk_dataanalys/binormalfördelning): add comments and variable r

--- statistisk_dataanalys/binormalfördelning.py
+++ statistisk_dataanalys/binormalfördelning.py
@@ -3,8 +3,17 @@ from scipy.stats import binom
 # Parameters for the binomial distribution
 n = 20
 p = 0.6
+r = 12
 
-# Calculating P(X <= 12)
-prob_X_leq_12 = binom.cdf(12, n, p)
-print(prob_X_leq_12)
+
+# prob_X_greater_than_r = 1 - binom.cdf(r, n, p)
+# prob_X_greater_than_or_equal_r = 1 - binom.cdf(r - 1, n, p)
+
+# prob_X_less_than_r = binom.cdf(r - 1, n, p)
+# prob_X_less_than_or_equal_r = binom.cdf(r, n, p)
+
+
+
+prob_X_leq_r = binom.cdf(r, n, p)
+print(prob_X_leq_r)
 

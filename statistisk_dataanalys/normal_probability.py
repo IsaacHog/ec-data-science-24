386e5dd586cec4f40627359772a78c2fcea25e99
IsaacHog <isaac.hogfeldt@hotmail.com>
2024-11-05 13:10:01 +0100

    fix(statistisk_dataanalys): add range functionality

--- statistisk_dataanalys/normal_probability.py
+++ statistisk_dataanalys/normal_probability.py
@@ -3,9 +3,17 @@ import math
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Calcualte z 
+x = 14
+expected_value = 10
+standard_deviation = 2.5
+calculated_z = (x - expected_value) / standard_deviation
+print(f"calculated_z = {calculated_z}")
+
 # Parameters
-z = 2.2 # = (X − μ) / σ
-operator = "<" # Operator: '=', '<=', '<', '>', or '>='
+z_1 = 1.6 # (X − μ) / σ
+z_2 = 0.8 # None or upper bound
+operator = "<=" # Operator: '=', '<=', '<', '>', or '>='
 
 def normal_probability(z, operator):
     if operator == "<=":
@@ -21,19 +29,11 @@ def normal_probability(z, operator):
     else:
         raise ValueError("Invalid operator. Use '=', '<=', '<', '>', or '>='")
 
-z0 = normal_probability(z, operator)
-print(f"P(Z {operator} {z}) = {z0}")
-
-# Plotting the normal distribution
-x = np.linspace(-4, 4, 1000)
-y = norm.pdf(x) 
-
-plt.plot(x, y, label='Normal Distribution', color='blue')  
-plt.fill_between(x, y, where=(x <= z), color='lightblue', alpha=0.5, label=f'P(Z <= {z})')
-plt.axvline(z, color='red', linestyle='--', label=f'Z = {z}') 
-plt.title('Normal Distribution')
-plt.xlabel('Z')
-plt.ylabel('Probability Density')
-plt.legend()
-plt.grid()
-plt.show()
+if (z_2 == None):
+    z0 = normal_probability(z_1, operator)
+    print(f"P(Z {operator} {z_1}) = {z0}")
+else:
+    z0_1 = normal_probability(z_1, operator)
+    z0_2 = normal_probability(z_2, operator)
+    z0 = z0_1 - z0_2
+    print(f"P({z_1} <= Z <= {z_2}) = {z0}")
